e547cd09df31348754c453e233f5ecf85774e4ad
ecastrob <53960603+ecastrob@users.noreply.github.com>
2026-08-23 12:53:09 -0400

    Complete Home landing page and attribution update

--- tests/github-pages.test.mjs
+++ tests/github-pages.test.mjs
@@ -8,8 +8,11 @@ test("exports a GitHub Pages-ready static site", async () => {
   const html = await readFile(new URL("../out/index.html", import.meta.url), "utf8");
 
   assert.match(html, /<title>B Co 2-485 Soldier Guide<\/title>/i);
-  assert.match(html, />All Guides</i);
-  assert.match(html, /Created by an NCO for Reserve Soldiers and NCOs/i);
+  assert.match(html, />Home</i);
+  assert.match(html, /Welcome to the B Co 2-485 Soldier Guide/i);
+  assert.doesNotMatch(html, />All Guides</i);
+  assert.match(html, /Created by an NCO for Reserve Soldiers\./i);
+  assert.doesNotMatch(html, /Reserve Soldiers and NCOs/i);
   assert.match(html, new RegExp(`${repositoryPath}/_next/`));
   assert.match(html, new RegExp(`${repositoryPath}/favicon\\.svg`));
   assert.match(html, /https:\/\/ecastrob\.github\.io\/Bco-2-485-guide/i);
